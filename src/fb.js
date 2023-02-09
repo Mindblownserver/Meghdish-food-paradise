75bf23e0b5eb916efa4fff00307182e08e0d2c54
Mohamed Yassine Kharrat <30394173+Mindblownserver@users.noreply.github.com>
2023-02-09 16:54:46 +0100

    Update fb.js

--- src/fb.js
+++ src/fb.js
@@ -2,13 +2,7 @@ import firebase from 'firebase/app'
 import 'firebase/firestore'
 
 var firebaseConfig = {
-    apiKey: "AIzaSyBdCaVqWmb1Ay2rIuTiHyU0Um7VlT7GElQ",
-    authDomain: "meghdish-food-paradise.firebaseapp.com",
-    projectId: "meghdish-food-paradise",
-    storageBucket: "meghdish-food-paradise.appspot.com",
-    messagingSenderId: "803181228362",
-    appId: "1:803181228362:web:426807645bf2f22a9811b3",
-    measurementId: "G-4J04QEBC0C"
+    
 };
 
 firebase.initializeApp(firebaseConfig);
